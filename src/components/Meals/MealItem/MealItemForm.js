d73b93937c07088b2d56fd3a17916267a95e092d
BadiuIoana <badioana9898@gmail.com>
2023-09-13 10:18:16 +0300

    Adding refs for inputs

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,11 +1,28 @@
 import React from "react";
 import classes from "./MealItemForm.module.css";
 import Input from "../../UI/Input";
-
+import { useRef, useState } from "react";
 const MealItemForm = (props) => {
+    const amountInputRef = useRef();
+    const [amountIsValid, setAmountIsValid] = useState(true);
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const enteredAmount = amountInputRef.current.value; // THIS VALUE IS ALWAYS A STRING!
+        const enteredAmountNumber = +enteredAmount;
+        if (
+            enteredAmount.trim().lenght === 0 ||
+            enteredAmountNumber < 1 ||
+            enteredAmount > 5
+        ) {
+            setAmountIsValid(false);
+            return;
+        }
+        props.onAddToCart(enteredAmountNumber);
+    };
     return (
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={submitHandler}>
             <Input
+                ref={amountInputRef}
                 label='Amount'
                 input={{
                     id: "amount_" + props.id,
@@ -17,6 +34,7 @@ const MealItemForm = (props) => {
                 }}
             />
             <button>+Add</button>
+            {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
         </form>
     );
 };
