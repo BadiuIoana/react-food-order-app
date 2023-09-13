52d55947d058af0ac69892d93e750116f23f118b
BadiuIoana <badioana9898@gmail.com>
2023-09-13 15:42:55 +0300

    Added checkout form

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,13 @@
 import React from "react";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartItem from "./CartItem";
+import Checkout from "./Checkout";
 
 const Cart = (props) => {
+    const [isCheckout, setIsCheckout] = useState(false);
     const cartCtx = useContext(CartContext);
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
     const hasItems = cartCtx.items.length > 0;
@@ -16,6 +18,10 @@ const Cart = (props) => {
 
     const cartItemAddHandler = (item) => {
         cartCtx.addItem({ ...item, amount: 1 });
+    };
+
+    const orderHanler = () => {
+        setIsCheckout(true);
     };
     const cartItems = (
         <ul className={classes["cart-items"]}>
@@ -39,15 +45,25 @@ const Cart = (props) => {
                 <span>Total Amount</span>
                 <span>{totalAmount}</span>
             </div>
-            <div className={classes.actions}>
-                <button
-                    className={classes["button--alt"]}
-                    onClick={props.onClose}
-                >
-                    Close
-                </button>
-                {hasItems && <button className={classes.button}>Order</button>}
-            </div>
+            {isCheckout && <Checkout onCancel={props.onClose} />}
+            {!isCheckout && (
+                <div className={classes.actions}>
+                    <button
+                        className={classes["button--alt"]}
+                        onClick={props.onClose}
+                    >
+                        Close
+                    </button>
+                    {hasItems && (
+                        <button
+                            className={classes.button}
+                            onClick={orderHanler}
+                        >
+                            Order
+                        </button>
+                    )}
+                </div>
+            )}
         </Modal>
     );
 };
